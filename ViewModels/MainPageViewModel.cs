f79515606584c56ea4704039cdfbcd4cc1f7468c
Richard Woollcott <richard.woollcott@appmilla.co.uk>
2024-06-18 10:02:07 +0100

    Refactored main page view model to simplify/more functional

--- ViewModels/MainPageViewModel.cs
+++ ViewModels/MainPageViewModel.cs
@@ -21,31 +21,25 @@ public class MainPageViewModel : ReactiveObject, IActivatableViewModel
     private readonly ICrewRepository _crewRepository;
     private readonly INavigationService _navigationService;
     private readonly IUserAlerts _userAlerts;
-    
+
     [Reactive]
-    public string PageTitle { get; set; }
-    
+    public string PageTitle { get; private set; } = "People In Space Functional MAUI";
+
     [ObservableAsProperty]
     public bool IsRefreshing { get; }
-    
+
     public ReactiveCommand<bool, Either<CrewError, IReadOnlyList<CrewModel>>> LoadCommand { get; }
-    
+
     public ReactiveCommand<CrewModel, Unit> NavigateToDetailCommand { get; private set; }
-    
-    private ReadOnlyObservableCollection<CrewModel> _crew;
 
-    public ReadOnlyObservableCollection<CrewModel> Crew
-    {
-        get => _crew;
-        set => this.RaiseAndSetIfChanged(ref _crew, value);
-    }
+    public ReadOnlyObservableCollection<CrewModel> Crew { get; private set; }
 
-    private static readonly Func<CrewModel, string> KeySelector = crew => crew.Id;
-    private readonly SourceCache<CrewModel, string> _crewCache = new(KeySelector);
-        
+    private readonly SourceCache<CrewModel, string> _crewCache = new(crew => crew.Id);
+
     public ViewModelActivator Activator { get; } = new();
-    
-    public MainPageViewModel(ISchedulerProvider schedulerProvider,
+
+    public MainPageViewModel(
+        ISchedulerProvider schedulerProvider,
         ICrewRepository crewRepository,
         INavigationService navigationService,
         IUserAlerts userAlerts)
@@ -54,30 +48,39 @@ public class MainPageViewModel : ReactiveObject, IActivatableViewModel
         _crewRepository = crewRepository;
         _navigationService = navigationService;
         _userAlerts = userAlerts;
-        
-        PageTitle = "People In Space Functional MAUI";
-        
-        var crewSort = SortExpressionComparer<CrewModel>
-            .Ascending(c => c.Name);
+
+        var crewSort = SortExpressionComparer<CrewModel>.Ascending(c => c.Name);
 
         var crewSubscription = _crewCache.Connect()
             .Sort(crewSort)
-            .Bind(out _crew)
+            .Bind(out var crew)
             .ObserveOn(_schedulerProvider.MainThread)        
             .DisposeMany()                              
             .Subscribe();
-        
+
+        Crew = crew;
+
         LoadCommand = ReactiveCommand.CreateFromObservable<bool, Either<CrewError, IReadOnlyList<CrewModel>>>(
-            forceRefresh =>  _crewRepository.GetCrew(forceRefresh),
-            this.WhenAnyValue(x => x.IsRefreshing).Select(x => !x), 
-            outputScheduler: _schedulerProvider.MainThread); 
-        LoadCommand.ThrownExceptions.Subscribe(Crew_OnException);
-        LoadCommand.Subscribe(Crew_OnNext);
-        
+            crewRepository.GetCrew,
+            outputScheduler: _schedulerProvider.MainThread);
+
+        LoadCommand.ThrownExceptions.Subscribe(ex => ShowError(ex.Message));
+        LoadCommand.Subscribe(result => result.Match(
+            Right: UpdateCrew,
+            Left: error => ShowError(error.Message)));
+
         NavigateToDetailCommand = ReactiveCommand.Create<CrewModel>(NavigateToDetail);
-        
+
         this.WhenActivated(disposables =>
         {
+            /*
+            LoadCommand.IsExecuting.ToPropertyEx(
+                    this,
+                    x => x.IsRefreshing,
+                    scheduler: _schedulerProvider.MainThread)
+                .DisposeWith(disposables);*/
+            
+            
             this.WhenAnyValue(x => x._crewRepository.IsBusy)
                 .ObserveOn(_schedulerProvider.MainThread)
                 .ToPropertyEx(this, x => x.IsRefreshing, scheduler: _schedulerProvider.MainThread)
@@ -86,58 +89,26 @@ public class MainPageViewModel : ReactiveObject, IActivatableViewModel
             disposables.Add(crewSubscription);
         });
     }
-    
-    private void Crew_OnNext(Either<CrewError, IReadOnlyList<CrewModel>> result)
-    {
-        result.Match(
-            crew =>
-            {
-                if (crew != null) UpdateCrew(crew);
-            },
-            Crew_OnError);
-    }
-    
+
     private void UpdateCrew(IReadOnlyList<CrewModel> crew)
     {
-        _crewCache.Edit(innerCache =>
-        {
-            innerCache.AddOrUpdate(crew);
-        });
-    }
-
-    private void Crew_OnException(Exception e)
-    {
-        ShowError(e.Message);
-    }
-    
-    private void Crew_OnError(CrewError e)
-    {
-        switch (e)
-        {
-            case NetworkError:
-                ShowError(e.Message);
-                break;
-            case ParsingError:
-                _userAlerts.ShowSnackbar(e.Message, TimeSpan.FromSeconds(5));
-                break;
-            default:
-                ShowError(e.Message);
-                break;
-        }
+        _crewCache.Edit(cache => cache.AddOrUpdate(crew));
     }
 
     private void ShowError(string message)
     {
         _userAlerts.ShowToast(message).FireAndForgetSafeAsync();
     }
-    
+
     private void NavigateToDetail(CrewModel crewMember)
     {
-        var name = Uri.EscapeDataString(crewMember.Name);
-        var image = Uri.EscapeDataString(crewMember.Image.ToString());
-        var wikipedia = Uri.EscapeDataString(crewMember.Wikipedia.ToString());
-        
-        var route = $"{Routes.DetailPage}?name={name}&image={image}&wikipedia={wikipedia}";
+        var parameters = new Dictionary<string, string>
+        {
+            ["name"] = Uri.EscapeDataString(crewMember.Name),
+            ["image"] = Uri.EscapeDataString(crewMember.Image.ToString()),
+            ["wikipedia"] = Uri.EscapeDataString(crewMember.Wikipedia.ToString())
+        };
+        var route = $"{Routes.DetailPage}?{string.Join("&", parameters.Select(p => $"{p.Key}={p.Value}"))}";
         _navigationService.NavigateAsync(route);
     }
-}   
+}
